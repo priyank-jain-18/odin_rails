0dd598f8da4056e6bec7ba66cf2f7f3ff9b6d49e
jibzoiderz <tapiador.jib@gmail.com>
2016-12-24 17:29:26 +0800

    completed basic structure of caesar cipher

---
+++ sinatra_proj/views/scripts/index.js
@@ -0,0 +1,20 @@
+$(document).ready( function()  {
+
+	$("#caesar_button").click(function() {
+		window.location.replace("caesar_cipher.html");
+	});
+
+	$("#hangman").click(function() {
+		window.location.replace("hang_man.html");
+	});
+
+
+
+
+
+
+
+
+
+
+});
