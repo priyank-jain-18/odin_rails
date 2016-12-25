ae9fc84b85d8a4e17e4eac78d8eb4f1ea3a5949c
jibzoiderz <tapiador.jib@gmail.com>
2016-12-25 20:37:00 +0800

    removed javascript features and switched it to ruby instead

--- sinatra_proj/caesar.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'caesar_cipher/caesar_cipher.rb'
-
-
--- sinatra_proj/main.rb
+++ sinatra_proj/main.rb
@@ -12,9 +12,25 @@ get '/caesar_cipher.html' do
 	erb:caesar_cipher, locals: {translation: translation}
 end
 
+get '/hang_man.html' do
+	erb:hang_man
+end
+
 post '/runCaesar' do 	
     translation = caesar_cipher(params["str"].to_s, params["vals"].to_i)
     erb:caesar_cipher, locals: {translation: translation}
+end
+
+post '/backMenu' do
+	redirect '/'
+end
+
+post '/caesar_but' do
+	redirect 'caesar_cipher.html'
+end
+
+post '/hangman_but' do
+	redirect 'hang_man.html'
 end
 
 
