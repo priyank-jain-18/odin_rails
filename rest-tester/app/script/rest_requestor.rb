e1e50bb1a9eebaba892b2dd16ef828469fafa903
jibzoiderz <tapiador.jib@gmail.com>
2017-01-06 18:40:04 +0800

    finished the tutorial

--- rest-tester/app/script/rest_requestor.rb
+++ rest-tester/app/script/rest_requestor.rb
@@ -7,5 +7,5 @@ url = "http://localhost:3000/users/edit"
 puts RestClient.get(url)
 url = "http://localhost:3000/users/new"
 puts RestClient.get(url)
-url = "http://localhost:3000/users/create"
-RestClient.post(url, {params: {name: "harmabe did 9/11"}})
+url = "http://localhost:3000/users"
+RestClient.post(url, name: "harmabe did 9/11")
