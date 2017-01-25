4e51bfcb664c1bcd7cdc836adb54226e856d8422
jibzoiderz <tapiador.jib@gmail.com>
2017-01-26 02:09:54 +0800

    finished gettign_started

--- getting_started/blog/app/controllers/articles_controller.rb
+++ getting_started/blog/app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+
+	http_basic_authenticate_with name: "ggez", password: "eeeeazy",
+	except: [:index, :show]
+
 	def index
 		@articles = Article.all 
 	end
--- getting_started/blog/app/models/comment.rb
+++ getting_started/blog/app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :username, presence: true
+  validates :body, presence: true, length: {minimum: 5}
+
+
 end
