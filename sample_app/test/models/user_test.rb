defd21959517908ff59318361a4bb220623152cc
jibzoiderz <tapiador.jib@gmail.com>
2017-01-28 22:18:20 +0800

    finished basic user model including passwords

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+ 
   def setup 
-  	@user = User.new(name: "Example User", email: "user@example.com")
+  	@user = User.new(name: "Example User", email: "user@example.com",
+  		password: "foobar", password_confirmation: "foobar")
   end
 
   test "it should be valid" do 
@@ -43,8 +44,15 @@ class UserTest < ActiveSupport::TestCase
   	assert_equal @user.email.downcase, @user.reload.email
   end
 
+  test "password should be present (nonblank)" do
+  	@user.password = @user.password_confirmation = " " * 6
+  	assert_not @user.valid?
+  end
 
-
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 
   	
 end
