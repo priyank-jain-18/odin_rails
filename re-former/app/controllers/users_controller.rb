187e098cdced77195ae01a835998dc86d3f67ec8
jibzoiderz <tapiador.jib@gmail.com>
2017-02-01 14:22:24 +0800

    improved forms

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -5,16 +5,20 @@ class UsersController < ApplicationController
 	end
 
 	def create
-	end
+		@user = User.new(user_params)
 
+		if @user.save
+			redirect_to new_user_path
+		else
+			render :new
+		end
+	end
 
 	private
 
-	def user_params
-
-
+	def user_params 
+		params.require(:user).permit(:username,:email,:password,:password_confirmation)
 	end
-
 
 
 
