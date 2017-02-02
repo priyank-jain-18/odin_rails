a883b4c78b26fb84a35a8dde99a6077f7109e31a
jibzoiderz <tapiador.jib@gmail.com>
2017-02-02 20:17:01 +0800

    added gravatar img feature

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
   
 end
