87d128e25956099f34f57abe19fbace3c9ece4cc
jibzoiderz <tapiador.jib@gmail.com>
2017-02-03 01:28:10 +0800

    added users signup test

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -11,8 +11,7 @@ Rails.application.routes.draw do
 
 
   get '/signup', to: 'users#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
 
   resources :users
   
