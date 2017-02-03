d9d55c5876fc0a97c1f3ed4061c0e39f8c571b6b
jibzoiderz <tapiador.jib@gmail.com>
2017-02-04 02:28:52 +0800

    adding session login feature

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+
+  get 'sessions/new'
+
+
   get 'users/new'
+
 
   root 'static_pages#home'
 
@@ -12,6 +17,12 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 
   resources :users
   
