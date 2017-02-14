5b9f110d945f98db76d33db851a0a4a38d556643
jibzoiderz <tapiador.jib@gmail.com>
2017-02-15 02:05:46 +0800

    Completed basic User model including secure passwords

---
+++ sample_app_V2/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+	before_save {self.email.downcase!}
+
+	validates :name,presence: true,length: {maximum: 50}
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	validates :email,presence: true,length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX},
+		uniqueness: {case_sensitive: false}
+
+	validates :password, length: {in: 6..75}, presence: true
+
+
+	has_secure_password
+end
---
+++ sample_app_V2/test/models/user_test.rb
@@ -0,0 +1,85 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+	def setup
+		@user = User.new(name: "Test User", email: "tester@gmail.com",
+			password: "tester123", password_confirmation: "tester123")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "blank name should not be valid" do
+		@user.name = "    "
+		assert_not @user.valid?		
+	end
+
+	test "blank email should not be valid" do
+		@user.email = "     "
+		assert_not @user.valid?
+	end
+
+	test "name should not be more than 50 characters" do
+		@user.name = 'x' * 51  #51 characters
+		assert_not @user.valid?
+	end
+
+	test "email should not be more than 255 characters" do
+		@user.email = 'x' * 244 + "@example.com" #this is 256 characters
+		assert_not @user.valid?
+	end
+
+	test "email should accept valid addresses" do
+		valid_addresses = %w[test@gmail.com TEST@gmail.CoM A_US-ER@gmail.bar.org
+                         first.last@gmail.jp alice+bob@gmail.cn]
+        
+        valid_addresses.each do |email_address|
+        	@user.email = email_address
+        	assert @user.valid?, "#{email_address} should be valid"
+        end
+    end
+
+    test "email should NOT accept INVALID addresses" do
+    	invalid_addresses = %w[test@gmail,com user_at_gmail.org user.name@gmail.com. 
+    		tester@gmail_asd.com test@test1+baz.com asdasdasdasd@gmail..com]
+
+    	invalid_addresses.each do |invalid_email|
+    		@user.email = invalid_email
+    		assert_not @user.valid?, "#{invalid_email} should be invalid"
+    	end
+    end
+
+    test "email should have no duplicates in the database" do
+ 		duplicate_user = @user.dup #dup means another duplicate user from setup
+ 		@user.save
+ 		assert_not duplicate_user.valid?
+
+ 		duplicate_user.email = @user.email.upcase #should be case insensitive 
+ 		assert_not duplicate_user.valid?, "should be case insensitive"
+ 	end
+
+ 	test "emails should be saved as downcase" do
+ 		@user.email = "TeSTEr@GmAIL.CoM"
+ 		@user.save
+
+ 		assert_equal @user.email.downcase, @user.reload.email
+ 	end
+
+ 	test "passwords should be invalid when blank" do
+ 		@user.password = @user.password_confirmation = "     "
+ 		assert_not @user.valid?
+ 	end
+
+ 	test "password should have a minimum length of 6" do
+ 		@user.password = @user.password_confirmation = 'x' * 5
+ 		assert_not @user.valid?
+ 	end
+
+ 	test "password should have a maximum length of 75" do
+ 		@user.password = @user.password_confirmation = 'x' * 76
+ 		assert_not @user.valid?
+ 	end
+
+end
