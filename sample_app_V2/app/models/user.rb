81032950a48c52aa3e89c1e5e10099c658aaedf3
jibzoiderz <tapiador.jib@gmail.com>
2017-02-17 17:27:33 +0800

    added some remember token features

--- sample_app_V2/app/models/user.rb
+++ sample_app_V2/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save {self.email.downcase!}
 
 	validates :name,presence: true,length: {maximum: 50}
@@ -12,9 +14,18 @@ class User < ApplicationRecord
 
 	has_secure_password
 
-	def User.digest(string)
+	def self.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     	return BCrypt::Password.create(string, cost: cost)
+    end
+
+    def self.new_token  #returns a random hash/token
+    	SecureRandom.urlsafe_base64
+    end
+
+    def remember #remembers a user in the data base
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
