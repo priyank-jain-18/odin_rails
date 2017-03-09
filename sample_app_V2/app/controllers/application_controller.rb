32efa4dd6aaa9ad95a28c6c98ee5b8c7d3c628be
jibzoiderz <tapiador.jib@gmail.com>
2017-03-09 22:26:03 +0800

    made sample app to submodule

--- sample_app_V2/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  include SessionsHelper #this was included so that session helper is available to all and will enable logging in
-end
